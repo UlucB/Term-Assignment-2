c931a12ac1ef4ae2ef7823ce1764005e4c94c8ee
Uluc <ubulbirol@yahoo.com>
2021-11-21 05:20:29 -0800

    View Page functions added

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// Index Page
+// ACIT 1620 - Set B - Assignment 2
+// Created by: Luke Birol - A01294098
+// Created: Nov 20, 2021
 
-//     Create a function called cleanUpIndex that removes all of the DOM nodes that are unique to the Index page. This function takes no parameters.
+// Index Page 
 
 function cleanUPIndex() {
     let main = document.getElementsByClassName('main');
     all_a = main[0].querySelectorAll('a')
     for (let i = 0; i < all_a.length; i++) {all_a[i].remove();};
 }
-// ^^^This seems to be what the question is asking for. By keeping div.main, this allows renderIndex(array) to dump new index without cleanUpIndex() being run first; the new cards are appened to already-existing div.main instead of using a new div.main.
+// ^^^This seems to be what the question is asking for. By keeping div.main, this allows renderIndex(array) to dump in new indicies without cleanUpIndex() being run first; the new cards are appened to already-existing div.main instead of using a new div.main.
 
-//     Create a function called createSingleIndex that creates a DOM node that represents a single index card for the Index page. This function will take in a single object, where that object represents a single contact (see examples below). It should output a DOM node. It does not need to attach the DOM node to the rest of the DOM.
 function createSingleIndex(single_object) {
     let new_a = document.createElement('a');
     new_a.href="page3.html";
@@ -23,8 +24,6 @@ function createSingleIndex(single_object) {
     return new_a
 }
 
-//     Create a function called renderIndex that creates all of the DOM nodes that are unique to the Index page. This function will take in a single parameter, which is an array containing many contacts, each of which represents a single contact. An example array of contacts is provided below. But be warned, I will supply the array, and I may change the details, like how many contacts there are. Obviously the reason you wrote createSingleIndex is because I thought it would help you write this function. Unlike createSingleIndex, which just sort of creates some stuff in isolation, this function must actually put DOM nodes onto the web page.
-
 function renderIndex(array) {
     let main_div=document.getElementsByClassName('main')
 
@@ -33,7 +32,6 @@ function renderIndex(array) {
         main_div[0].appendChild(new_a);
     }
 }
-
 
 // function vanilla_renderIndex(array) {
 //     let main_div=document.getElementsByClassName('main')
@@ -52,61 +50,80 @@ function renderIndex(array) {
 //     }
 // }
 
-
-
-// /* Here is an example of a contact list array, with two contacts already populated */
-let contactList = [
-  {
-    name: "Roberta Dobbs",
-    phone: "778-555-1234",
-    address: "101 Main St, Anytown, USA",
-    email: "subgenius@slack.example.com",
-  }, 
-  {
-    name: "Bugs Bunny",
-    phone: "123-867-5309",
-    address: "Warner Brothers Animation Lot",
-    email: "whatsup@doc.example.com",
-  },
-    {
-    name: "Roberta Dobbs",
-    phone: "778-555-1234",
-    address: "101 Main St, Anytown, USA",
-    email: "subgenius@slack.example.com",
-  }, 
-  {
-    name: "Bugs Bunny",
-    phone: "123-867-5309",
-    address: "Warner Brothers Animation Lot",
-    email: "whatsup@doc.example.com",
-  },
-]
-
-// How you can tell it's working
-
-// If you go to an index page, and you open the browser console, you should be able to:
-
-//     call cleanUpIndex() to delete all the index cards
-//     call renderIndex(contactList) to put them all back
-//         the page should be exactly the same as it was before!
-//     rinse and repeat
-
 // View Page
 
-// This is the page where we view a single contact that we created previously.
-
-// //     Create a function called cleanUpView that removes all of the DOM nodes that are unique to the View page. This function takes no parameters.
 function cleanUpView() {
-    let all = document.getElementsByClassName('contactinfo');
-    for (let i = 0; i < all.length; i) {all[i].remove();};
+    let main = document.getElementsByClassName('main');
+    all_div = main[0].querySelectorAll('div');
+    for (let i = 0; i < all_div.length; i++) {all_div[i].remove();};
 }
 
-// } 
-// // //     Create a function called renderView that creates all of the DOM nodes that are unique to the View page. This function will take in a single parameter, which is an object that represents a single contact. Much like renderIndex, this function is reponsible for actually modifying the web page.
-// function renderView(single_contact) {
-//     let para = document.querySelectorAll('p')
-//     return para, single_contact
-// } 
+function renderView(single_contact) {
+    let contact_info_node = document.createElement('div');
+    contact_info_node.classList.add('contactinfo'); 
+
+    // name + image
+    let name_img_node = document.createElement('div');
+    name_img_node.classList.add('contactname'); 
+
+    let name = document.createTextNode(single_contact['name']);
+    name_img_node.appendChild(name);
+    
+    let image = document.createElement('img');
+    image.classList.add('profilepic');
+    image.src = "./img/profile.jpg";
+    image.alt = "Profile picture";
+    name_img_node.appendChild(image);
+
+    contact_info_node.appendChild(name_img_node);
+
+    //email
+    let email_node = document.createElement('div');
+    email_node.classList.add('contactemail'); 
+    let email = document.createTextNode("email: " + single_contact['email']);
+    email_node.appendChild(email);
+    contact_info_node.appendChild(email_node);
+
+    // phone
+    let phone_node = document.createElement('div');
+    phone_node.classList.add('contactphone'); 
+    let phone = document.createTextNode("cell: +1 " + single_contact['phone']);
+    phone_node.appendChild(phone);
+    contact_info_node.appendChild(phone_node);
+
+    // address
+    let address_node = document.createElement('div');
+    address_node.classList.add('contactaddress'); 
+    let address = document.createTextNode("address: " + single_contact['address']);
+    address_node.appendChild(address);
+    contact_info_node.appendChild(address_node);
+
+    let main_class = document.getElementsByClassName('main');
+    let main = main_class[0];
+
+    //buttons
+    let button_node = document.createElement('div');
+    button_node.classList.add('buttons');
+
+    let edit_button = document.createElement('button');
+    edit_button.classList.add('button', 'edit');
+    edit_button.value= "Edit";
+    let edit = document.createTextNode("Edit");
+    edit_button.appendChild(edit);
+    button_node.appendChild(edit_button);
+
+    let close_button = document.createElement('button');
+    close_button.classList.add('button', 'close');
+    close_button.value= "Close";
+    let close = document.createTextNode("Close");
+    close_button.appendChild(close);
+    button_node.appendChild(close_button);
+
+    contact_info_node.appendChild(button_node);
+
+    //append all of the above into div.main
+    main.appendChild(contact_info_node);
+}
 
 // How you can tell it's working
 
@@ -117,7 +134,7 @@ function cleanUpView() {
 //         the page should be exactly the same as it was before!
 //     rinse and repeat
 
-// Create Page
+// Create Page (page2)
 
 // I hope the pattern is pretty obvious at this point.
 
@@ -129,11 +146,13 @@ function cleanUpCreate() {
 
 // }
 // //     Create a function called renderCreate that creates all of the DOM nodes that are unique to the Create page. This function will take in a single parameter, which is an object that represents a single contact. Much like renderIndex, this function is reponsible for actually modifying the web page.
-// function renderCreate(single_contact) {
-//     return single_contact
 
 
-// }
+function renderCreate(single_contact) {
+    return single_contact
+
+
+}
 
 
 
@@ -176,3 +195,25 @@ function cleanUpCreate() {
 // If you want to take it farther, here's what didn't make the cut for this assignment. You could obviously add another page called Edit that looks just like Create, and for that page create another set of functions for editing. It's sort of the same as the Create page, but it takes in an existing contact object, and pre-populates all the fields. It also would say "Update" or something on the button, instead of "Save".
 
 // No, I'm not promising any bonus marks for this, mostly because it makes the marking a mess. The bonus is in your heart.
+
+// /* Here is an example of a contact list array, with two contacts already populated */
+let contactList = [
+    {
+      name: "Roberta Dobbs",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "subgenius@slack.example.com",
+    }, 
+    {
+      name: "Bugs Bunny",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "whatsup@doc.example.com",
+    },
+      {
+      name: "Jessica Rabbit",
+      phone: "236-420-6969",
+      address: "Patty-Cake, Toontown, USA",
+      email: "imnotbad@imjustdrawn.thisway.com",
+    }, 
+  ]
